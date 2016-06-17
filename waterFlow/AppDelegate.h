08ab995df4e784fb33e45ff5cb99b7014a9993d2
Roc.Tian <roctian@yaomaiche.com>
2016-06-17 10:15:17 +0800

    Initial Commit

---
+++ waterFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  waterFlow
+//
+//  Created by roctian on 16/6/17.
+//  Copyright © 2016年 roctian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ waterFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  waterFlow
+//
+//  Created by roctian on 16/6/17.
+//  Copyright © 2016年 roctian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
